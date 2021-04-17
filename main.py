e2fdf8bafd8318742d3050c8264a9e3a5a44723d
wilson <wilson.sfj@outlook.com>
2021-04-17 14:20:02 -0300

    creating candle after x minutes

--- main.py
+++ main.py
@@ -15,47 +15,53 @@
 
 import json
 import time
+import threading
+from datetime import datetime
 from websocket import create_connection
 
-class CandleSticker:
-    def create_clandle_stick(self, minutes):
-        prices = self.listen_forever(minutes=minutes)
-        converted_criptos = [float(i) for i in prices]
+class CandleSticker(threading.Thread):
 
-        open_value = converted_criptos[0]
-        close_value = converted_criptos[-1]
-        max_value = max(converted_criptos)
-        min_value = min(converted_criptos)
+    def __init__(self, minutes, **kwargs):
+        super(CandleSticker, self).__init__(**kwargs)
+        self.minutes = minutes
 
-        print(f'{minutes} minuto(s) - abertura: {open_value}, fechamento: {close_value}, maxima: {max_value}, minimo: {min_value}')
+    def run(self):
+        while True:
+            prices = self.listen_forever()
+            converted_criptos = [float(i) for i in prices]
 
+            open_value = converted_criptos[0]
+            close_value = converted_criptos[-1]
+            max_value = max(converted_criptos)
+            min_value = min(converted_criptos)
+            date_hour = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
-    def listen_forever(self, minutes):
+            print(f'{self.minutes} minuto(s) - abertura: {open_value}, fechamento: {close_value}, maxima: {max_value}, minimo: {min_value}, data-hora: {date_hour}')
+
+    def listen_forever(self):
         try:
             ws = create_connection('wss://api2.poloniex.com')
             ws.send('{"command": "subscribe", "channel": "USDT_BTC"}')
             
             price = []
-            t_end = time.time() + 60 * minutes
+            t_end = time.time() + 60 * self.minutes
             while time.time() < t_end:
                 result = ws.recv()
                 result = json.loads(result)
 
-                result = result[2][0]
-                if len(result) != 2:
-                    price.append(result[2])
+                if len(result) > 1:
+                    result = result[2][0]
+                    if len(result) != 2:
+                        price.append(result[2])
 
             ws.close()
             return price
         except Exception as ex:
             print(f'exception: {ex}')
 
-
 try:
-    cancle_sticker = CandleSticker()
-    while True:
-        cancle_sticker.create_clandle_stick(1)
-        cancle_sticker.create_clandle_stick(2)
-        cancle_sticker.create_clandle_stick(3)
+    CandleSticker(1).start()
+    CandleSticker(5).start()
+    CandleSticker(10).start()
 except Exception as e:
     print(f'Exception occured: {e}')

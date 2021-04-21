e516196897789ff30b518235ca061141e891bf21
Wilson Soares <wilson.sfj@outlook.com>
2021-04-21 19:59:07 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123@localhost/smarttbot_db'
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://smart:root@db/smarttbot_db'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123@localhost/smarttbot_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://smart:root@db/smarttbot_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
